e5169639b2d8133540e3cb5ff084b56d9173b5e7
Victoria <gubanova.vi10@gmail.com>
2021-11-12 19:13:48 +0300

    unused remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { PageContainer } from './components/PageContainer';
+import { LoginPage } from './pages/LoginPage/LoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <Router>
+      <div className="App">
+          <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/login" element={<LoginPage />}/>
+              <Route
+                  path="/register"
+                  element={
+                      <PageContainer>
+                          <p>Быть может доработаем в будущем</p>
+                      </PageContainer>
+                  }
+              />
+              <Route
+                  path="/forgot"
+                  element={
+                    <PageContainer>
+                        <p>
+                            Забывать пароли - плохо, но есть тестовый юзер:
+                            <br />
+                            <br />
+                            <code>Email: test@email.com</code>
+                            <br />
+                            <code>Password: password</code>
+                        </p>
+                    </PageContainer>
+                  }
+              />
+          </Routes>
+      </div>
+  </Router>
+);
