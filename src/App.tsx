08d9a326246e0e5a37215d7be888f8b4c02a4c9c
Victoria <gubanova.vi10@gmail.com>
2021-11-12 19:30:30 +0300

    for pages serve

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { PageContainer } from './components/PageContainer';
 import { LoginPage } from './pages/LoginPage/LoginPage';
 
@@ -7,7 +7,7 @@ export const App = () => (
   <Router>
       <div className="App">
           <Routes>
-              <Route path="" element={<LoginPage />} />
+              <Route path="/" element={<LoginPage />} />
               <Route path="/login" element={<LoginPage />}/>
               <Route
                   path="/register"
